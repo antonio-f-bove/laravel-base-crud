49da3cbeda0aad886584d4c1793b7679fa31ff43
Antonio Bove <antoniofbove@gmail.com>
2022-04-26 14:46:20 +0200

    - migration: create_comics_table.php

---
+++ database/migrations/2022_04_26_120230_create_comics_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateComicsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->char('title', 50);
+            $table->text('description');
+            $table->text('thumb');
+            $table->unsignedDecimal('price', 7, 2);     // comics can get quite pricy
+            $table->char('series', 50);
+            $table->date('sale_date');
+            $table->char('type', 50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+}
