f3bf6bbe368f39042efa5142a65a57a0898a60b7
Antonio Bove <antoniofbove@gmail.com>
2022-04-26 16:28:27 +0200

    index implementation

---
+++ resources/views/comics/index.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section('metaTitle', 'Comics')
+
+@section('main')
+  <table>
+    <thead>
+      <th>id</th>
+      <th>title</th>
+      <th>description (preview)</th>
+      <th>thumbnail url</th>
+      <th>price</th>
+      <th>sale_date</th>
+    </thead>
+    <tbody>
+      @foreach ($comics as $comic)
+      <tr>
+        <td>{{ $comic->id }}</td>
+        <td>{{ $comic->title }}</td>
+        <td>{{ substr($comic->description, 0, 50) . '...' }}</td>
+        <td>{{ substr($comic->thumb, 0, 50) . '...' }}</td>
+        <td>{{ $comic->price }}</td>
+        <td>{{ $comic->sale_date }}</td>
+      </tr>
+      @endforeach
+    </tbody>
+  </table>
+@endsection
---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('metaTitle', 'Homepage')
+    
+@section('main')
+  <main>
+    <h1>Homepage</h1>
+  </main>
+@endsection
